3df39dc796aefb60e269ce1918a5f3d7fbc6ee91
BobIT37 <boblerry4@gmail.com>
2020-02-25 23:47:25 -0500

    DragDrop was added

---
+++ SeleniumMavenProject2020/src/main/java/SeleniumMethods/CustomCSSPractice.java
@@ -0,0 +1,17 @@
+package SeleniumMethods;
+
+public class CustomCSSPractice {
+
+	public static void main(String[] args) {
+		
+		// #username
+		// #password
+		
+		//  input[id*='username'] == contains
+		//  div>input[id='username'] == parent
+		//  input[id$='username']  == ending
+		//  input[id^='username']
+
+	}
+
+}
